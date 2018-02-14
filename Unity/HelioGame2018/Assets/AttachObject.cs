b98909e2d35c23405c9385ae6e25f73150fa3db9
Anthony Romrell <anthony@artisandream.com>
2018-02-13 19:38:25 -0700

    Fish and other updates

---
+++ Unity/HelioGame2018/Assets/AttachObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachObject : MonoBehaviour {
+
+	public GameObject GO;
+	void OnMouseDown()
+	{
+		transform.parent = GO.transform;
+	}
+
+}
