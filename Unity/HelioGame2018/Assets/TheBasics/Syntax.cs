c147a723c53875730347145ada37cd4aa8c56586
Anthony Romrell <anthony@artisandream.com>
2018-02-20 21:26:40 -0700

    Updated
    Chess Game

---
+++ Unity/HelioGame2018/Assets/TheBasics/Syntax.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Syntax : MonoBehaviour
+{
+
+	//Value Types
+	public string UserName;
+	public int MyInt; 
+	private int _scoreTemp; 
+	public float Health = 0.0001f;
+	public float NewHealth;
+
+	//Reference Types
+	public GameObject MyGameObject;
+	public Transform MyTransform;
+	public Collider MyCollider;
+	
+	
+ 	void Run ()
+ 	{
+ 		//ScoreTemp Changed
+ 		
+ 	
+ 	}
+ }
