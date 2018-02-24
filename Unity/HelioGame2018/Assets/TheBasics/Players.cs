634b4ca14eba9a58e4ed91bf36570f3bf4aa6c2a
Anthony Romrell <anthony@artisandream.com>
2018-02-24 11:15:11 -0700

    Updated submodule Unity/HelioGame2018/Assets/Chess

---
+++ Unity/HelioGame2018/Assets/TheBasics/Players.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Players : ScriptableObject
+{
+
+	public List<string> PlayerNames;
+
+	public void AddPlayers(string name)
+	{
+		PlayerNames.Add(name);
+	}
+
+}
--- Unity/HelioGame2018/Assets/TheBasics/Syntax.cs
+++ Unity/HelioGame2018/Assets/TheBasics/Syntax.cs
@@ -1,28 +1,58 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using RoboRyanTron.Unite2017.Events;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Syntax : MonoBehaviour
 {
 
 	//Value Types
-	public string UserName;
-	public int MyInt; 
-	private int _scoreTemp; 
-	public float Health = 0.0001f;
-	public float NewHealth;
+	public string PlayerName;
+	public int Score = 100; 
+	public float Health = 1.0f;
+	public bool On = true;
 
 	//Reference Types
-	public GameObject MyGameObject;
-	public Transform MyTransform;
-	public Collider MyCollider;
+	public Players GamePlayers;
+
+	public Text InpuText;
 	
-	
- 	void Run ()
+ 	void Start ()
  	{
- 		//ScoreTemp Changed
+		 if (Score >= 0)
+		 {
+			 print(Score);
+		 }
+
+		 if (On)
+		 {
+			 print("On");
+		 }
+		 else
+		 {
+			 print("Off");
+		 }
+		 
+		 
+ 		Loops();
  		
- 	
  	}
+
+	void Loops()
+	{
+	
+		foreach (var player in GamePlayers.PlayerNames)
+		{
+			print(player + " is playing.");
+		}
+		
+	}
+
+	
+	public void AddPlayer()
+	{
+		GamePlayers.AddPlayers(InpuText.text);
+	}
  }
