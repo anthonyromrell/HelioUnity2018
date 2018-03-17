cda8a28ddaf5aee5d258808b36887179070d34d2
Anthony Romrell <anthony@artisandream.com>
2018-03-17 09:08:33 -0600

    UI Update

---
+++ Unity/HelioGame2018/Assets/TheBasics/UI/Scripts/UILocator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Made By Anthony Romrell
+public class UILocator : MonoBehaviour
+{
+	public Camera MyCamera;
+	public Vector3 CameraLocation;
+
+	private void Update()
+	{
+		print(MyCamera.pixelWidth);
+		CameraLocation.x = MyCamera.pixelWidth * 0.5f;
+		transform.localPosition = CameraLocation;
+	}
+}
